3c6538e5f7dfeca608f0128ffa91b6f068294fe7
Hihiz <public@mail.ru>
2022-12-27 22:00:09 +0300

    IoC

--- FileEncryptor.WPF/App.xaml.cs
+++ FileEncryptor.WPF/App.xaml.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Windows;
 
 namespace FileEncryptor.WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private static IHost __host;
+
+        public static IHost Host => __host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+
+            base.OnStartup(e);
+
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            using (Host) await Host.StopAsync();
+        }
     }
 }
