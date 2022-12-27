743e96894a83c15affcdf16d9cc872238c03c3b6
Hihiz <public@mail.ru>
2022-12-27 16:08:56 +0300

    Модель представления

---
+++ FileEncryptor.WPF/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FileEncryptor.WPF.VIewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
---
+++ FileEncryptor.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FileEncryptor.WPF.VIewModels.Base;
+
+namespace FileEncryptor.WPF.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Свойство Title - Заголовок окна
+
+        private string _title = "Шифратор";
+
+        public string Title { get => _title; set => Set(ref _title, value); }
+    }
+
+    #endregion
+}
